a0b1ceaf72133383d76bffb3528ddbb09262e52f
JavieraJimenezR <javiera.jimenez.rojas.99@gmail.com>
2023-07-18 16:14:44 -0400

    se arreglan rutas y herencia desde un html base

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-import os
-import hashlib
-import filetype
-import json
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
-from werkzeug.utils import secure_filename
-from database import db
+
+app = Flask(__name__)
+
+#Rutas de la app
+@app.route('/')
+def inicio():
+    return render_template('inicio.html')
+
+@app.route('/agregar-donacion', methods=['GET', 'POST'])
+def agregar_donacion():
+    if request.method == 'POST':
+        return 'Agregando donacion'
+    else:
+        return render_template('agregar-donacion.html')
+
+@app.route('/agregar-pedido', methods=['GET', 'POST'])
+def agregar_pedido():
+    if request.method == 'POST':
+        return 'Agregando pedido'
+    else:
+        return render_template('agregar-pedido.html')
+
+@app.route('/ver-donaciones', methods=['GET'])
+def ver_donaciones():
+    return render_template('ver-donaciones.html')
+
+@app.route('/ver-pedidos', methods=['GET'])
+def ver_pedidos():
+    return render_template('ver-pedidos.html')
